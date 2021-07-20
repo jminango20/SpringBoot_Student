eedf2d3639ab1dd0f330b67fafd2deac80e66619
Juan Minango <jcarlosminango@gmail.com>
2021-07-20 20:26:34 -0300

    Dependency Injection

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,6 +1,7 @@
 package com.example.demo.student;
 //Esta clase vai ter todos os Resources do API
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ public class StudentController {
 
     private final StudentService studentService;
 
+    @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
